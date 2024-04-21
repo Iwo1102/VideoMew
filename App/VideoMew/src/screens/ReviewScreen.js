1065a2f9e4da5593857ebaa53f64e8086939efa7
Nathan Perry <G00394355@atu.ie>
2024-04-21 21:29:05 +0100

    Added library functionality

--- App/VideoMew/src/screens/ReviewScreen.js
+++ App/VideoMew/src/screens/ReviewScreen.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import { View } from 'react-native';
+import CustomSearchButton from '../components/customSearchButton';
+import CustomTextBox from '../components/textBox';
 
-export default function ReviewScreen({navigation}) {
-    return(
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{fontSize: 26, fontWeight: 'bold'}}>Review</Text>
-            </View>
-    );
+export default function ReviewScreen({ navigation }) {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <CustomTextBox placeholder="Search" />
+      <CustomSearchButton onPress={() => console.log('Search pressed')} />
+    </View>
+  );
 }
